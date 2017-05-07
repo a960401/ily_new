118a00afd290e96c7064bb9fa485b1ef53e81169
찬하컴 <a960401@chanhakeomui-MacBook-Pro.local>
2017-05-07 14:34:39 +0900

    chanha2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170504154345) do
+ActiveRecord::Schema.define(version: 20170506095555) do
 
   create_table "bars", force: :cascade do |t|
     t.string   "title"
@@ -42,6 +42,11 @@ ActiveRecord::Schema.define(version: 20170504154345) do
   end
 
   create_table "posts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "schedules", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
