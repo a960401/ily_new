bbf90395fc33132e48f5416f3d1172bfc2cc0237
a960401 <a960401sogang@gmail.com>
2017-05-15 07:47:00 +0000

    almost final

--- db/migrate/20170504152218_create_foods.rb
+++ db/migrate/20170504152218_create_foods.rb
@@ -2,10 +2,10 @@ class CreateFoods < ActiveRecord::Migration[5.0]
   def change
     create_table :foods do |t|
       t.string :title
-      t.text :content
-      t.string :picture
-      t.integer :latitude
-      t.integer :longitude
+      t.string :content
+      t.string :imageurl
+      t.decimal :latitude, precision: 8, scale: 6
+      t.decimal	:longitude, precision: 9, scale: 6
       t.string :description
 
       t.timestamps
--- db/migrate/20170504152638_create_snacks.rb
+++ db/migrate/20170504152638_create_snacks.rb
@@ -2,11 +2,12 @@ class CreateSnacks < ActiveRecord::Migration[5.0]
   def change
     create_table :snacks do |t|
       t.string :title
-      t.string :section
+      #t.string :section
       t.text :content
-      t.string :picture
-      t.integer :latitude
-      t.integer :longitude
+      #t.string :picture
+      t.string :imageurl
+      t.decimal :latitude, precision: 8, scale: 6
+      t.decimal	:longitude, precision: 9, scale: 6
       t.string :description
       t.timestamps
     end
